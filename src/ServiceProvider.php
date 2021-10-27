e65996011edbee0195faedabf88c4effd1442d43
Arayik Smbatyan <arayiksmbatyan@gmail.com>
2021-10-27 10:53:31 +0400

    First Commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace HelloWorld;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/hello_world.php' => config_path('hello_world.php'),
+        ]);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/hello_world.php', 'hello_world'
+        );
+
+        $this->app->bind(HelloWorldService::class);
+    }
+}
---
+++ tests/test.php
@@ -0,0 +1,3 @@
+<?php
+
+\HelloWorld\HelloWorldService::print();
